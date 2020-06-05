c636e5548e024ce664aa5f00fe5c3ae14ff3c570
Stanislaw Hüll <huell@stud.uni-heidelberg.de>
2020-06-05 13:00:01 +0200

    --pre working

--- scripts/cds-wrapper.sh
+++ scripts/cds-wrapper.sh
@@ -1,47 +1,66 @@
 #!/usr/bin/env zsh
 
-# set -e
+# debug
+# set -- watch -d --pre="rm ./sqlite.db" --pre="cmd3 arg1 arg2"
 
 FILE=${0}
 
 usage() {
   echo "Lightweight wrapper around the cds command-line. Usage:
 
-  $FILE watch [ -d ] [ -e <FILE> ]
-  $FILE watch -l | --watchlist"
+  $FILE watch [ -d ] [ --pre=<COMMAND> ]
+  $FILE watch [ -l ]"
 
   exit
+}
+
+# return if a flag is set
+isflagset() {
+  echo "$FLAGS" | grep -qF "$1"
 }
 
 # returns watchlist
 watch/watchlist() {
   find '.' -type f                \
     -not -path '*/\.*'            \
-    -not -path './node_modules/*' \
+    -not -path '*/node_modules/*' \
     -not -name '*.db'
+}
+
+# parse cli options for watch subcommand
+watch/parseopt() {
+  shift
+  local -a opts
+  local -a precmds
+  zparseopts -a opts -- l d -pre+:=precmds
+
+  # concatenate all flags to a single string FLAGS
+  FLAGS=$(echo "$opts" | tr -d ' -')
+  echo "FLAGS=$FLAGS"
+
+  # store precmds as array PRECMDS
+  PRECMDS=()
+  for ((ii = 2; ii <= ${#precmds}; ii += 2)); do
+    PRECMDS+=$(echo $precmds[$ii] | sed 's/^=//g')
+  done
 }
 
 case "$1" in
   help|-h|--help)
     usage ;;
   watch)
+    watch/parseopt "$@"
 
-    # parse options
-    shift
-    local -a opts
-    zparseopts -a opts -- l d
-    params=$(echo "$opts" | tr -d ' -')
+    # -l | --watchlist: print watchlist end exit
+    isflagset l && watch/watchlist && exit
 
-    if echo "$params" | grep -qF 'l'; then
-      watch/watchlist
-      exit
-    else
-      entrcmd="cds watch"
-      if echo "$params" | grep -qF 'd'; then
-        entrcmd="cds deploy && $entrcmd"
-      fi
-      watch/watchlist | entr -rs "$entrcmd"
-    fi
+    # -d | --pre=<cmd>: build command string and run
+    entrcmd="cds watch"
+    isflagset d && entrcmd="cds deploy && $entrcmd"
+    for ((ii = ${#PRECMDS}; ii >= 1; ii -= 1)); do
+      entrcmd="$PRECMDS[$ii] && $entrcmd"
+    done
+    watch/watchlist | entr -rs "$entrcmd"
   ;;
   *)
     usage
